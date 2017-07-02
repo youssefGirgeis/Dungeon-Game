e374bac88582c22cf6eb1896f095a302855bcd72
Youssef Girgeis <youssefgirgeis@gmail.com>
2017-07-01 21:23:30 -0400

    add function to clear the screen

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,10 +1,14 @@
 import random
+import os
 
 CELLS = [(0,0), (1,0), (2,0), (3,0), (4,0),
 		(0,1), (1,1), (2,1), (3,1), (4,1),
 		(0,2), (1,2), (2,2), (3,2), (4,2),
 		(0,3), (1,3), (2,3), (3,3), (4,3),
 		(0,4), (1,4), (2,4), (3,4), (4,4)]
+
+def clear_sreen():
+	os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_locations():
 	monster = None
